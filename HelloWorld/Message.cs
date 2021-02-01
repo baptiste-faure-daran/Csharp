18d4a1f638e67c7d42e9f05f4e09442f89d9fb6d
Baptiste Faure Daran <baptiste.faure-daran@le-campus-numerique.fr>
2021-02-01 15:02:59 +0100

    initial commit

---
+++ HelloWorld/Message.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Message
+    {
+
+        string theDay = DateTime.Now.ToString("dddd");
+        string theTime = DateTime.Now.ToString("HH");
+        string userName = Environment.UserName;
+
+        public Message()
+        {
+
+
+        }
+
+        public void display()
+        {
+
+            if (theDay == "lundi" || theDay == "mardi" || theDay == "mercredi" || theDay == "jeudi")
+            {
+                if (theTime == "9" || theTime == "10" || theTime == "11" || theTime == "12")
+                {
+                    Console.WriteLine("Bonjour " + userName);
+
+                }
+                else if (theTime == "13" || theTime == "14" || theTime == "15" || theTime == "16" || theTime == "17")
+                {
+                    Console.WriteLine("Bon après-midi " + userName);
+
+                }
+                else
+                {
+                    Console.WriteLine("Bonsoir " + userName);
+
+                }
+
+            }
+            else if (theDay == "vendredi")
+            {
+
+                if (theTime == "9" || theTime == "10" || theTime == "11" || theTime == "12")
+                {
+                    Console.WriteLine("Bonjour " + userName);
+
+                }
+                else if (theTime == "13" || theTime == "14" || theTime == "15" || theTime == "16" || theTime == "17")
+                {
+                    Console.WriteLine("Bon après-midi " + userName);
+
+                }
+                else
+                {
+                    Console.WriteLine("Bon Weekend " + userName);
+
+                }
+
+            }
+
+
+        }
+
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Enter to start");
+
+            bool work = true;
+
+            while(work)
+            {
+                if(Console.ReadKey(true).Key == ConsoleKey.Enter)
+                {
+                    Message message = new Message();
+                    message.display();
+
+                }
+                Console.ReadLine();
+                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+                {
+                    Environment.Exit(0);
+
+                }
+            }
+
+            
+
+        }
+       
+    }
+}
